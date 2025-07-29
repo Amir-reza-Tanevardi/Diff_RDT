5d153c60670bf5f6aedb2f5a0e0b7662d3d32f2b
Amir-reza-Tanevardi <110600515+Amir-reza-Tanevardi@users.noreply.github.com>
2025-07-29 23:34:58 +0330

    Update RDT.py

--- RDT.py
+++ RDT.py
@@ -363,7 +363,7 @@ def train(config: TrainConfig, logger: Logger):
         state_std=dataset.state_std,
         reward_scale=config.reward_scale,
     )
-    env.reset(seed=config.seed)
+    env.seed(config.seed)
 
     # model
     model = set_model(config)
